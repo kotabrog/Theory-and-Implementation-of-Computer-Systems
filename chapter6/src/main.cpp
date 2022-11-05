d759e27748833a5b5cd4896086f6e81a1521108d
kotabrog <suzuki.kota0331@gmail.com>
2022-11-06 01:21:48 +0900

    complete non symbol

--- chapter6/src/main.cpp
+++ chapter6/src/main.cpp
@@ -1,12 +1,13 @@
 #include "../inc/Assembler.hpp"
 
-void fileNameCheck(std::string fileName)
+std::string fileNameCheck(std::string fileName)
 {
     std::size_t pos = fileName.rfind('.');
     if (pos == std::string::npos || fileName.substr(pos, 4) != ".asm") {
         std::cerr << "specify files with .asm extension" << std::endl;
         std::exit(1);
     }
+    return fileName.substr(0, pos) + ".hack";
 }
 
 int main(int argc, char** argv)
@@ -15,16 +16,20 @@ int main(int argc, char** argv)
         std::cerr << "specify a file name" << std::endl;
         std::exit(1);
     }
-    fileNameCheck(argv[1]);
+    std::string outPath = fileNameCheck(argv[1]);
+    // WIP error handling
+    std::ofstream ofs(outPath);
     Parser parser(argv[1]);
     while (parser.hasMoreCommands() && parser.advance()) {
-        std::bitset<16> line;
         if (parser.commandType() == CommandType::C_COMMAND) {
             Code code;
-            // std::bitset<3> dest = code.dest(parser.dest());
-            // std::bitset<7> comp = code.comp(parser.comp());
+            std::bitset<3> dest = code.dest(parser.dest());
+            std::bitset<7> comp = code.comp(parser.comp());
             std::bitset<3> jump = code.jump(parser.jump());
-            std::cout << parser.jump() << ": " << jump << std::endl;
+            ofs << "111" << comp << dest << jump << std::endl;
+        } else if (parser.commandType() == CommandType::A_COMMAND) {
+            std::bitset<15> symbol(std::stoi(parser.symbol()));
+            ofs << "0" << symbol << std::endl;
         }
     }
 }
